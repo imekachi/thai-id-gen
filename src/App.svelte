0e07e8536524a78faab133c1e13d720908a3e331
imekachi <imekachi@gmail.com>
2024-02-11 00:01:12 +0700

    feat: add pwa capability (#4)

--- src/App.svelte
+++ src/App.svelte
@@ -11,19 +11,19 @@
   <div
     class="h-[60dvh] grid place-content-center text-center text-violet-200 p-4"
   >
-    <h1 class="text-2xl font-semibold">Thai ID Gen</h1>
+    <h1 class="text-3xl font-semibold">Thai ID Gen</h1>
     <p>Generate and validate Thai ID number</p>
   </div>
-  <section class="p-6">
-    <h2 class="text-violet-200 font-medium mb-2">Generate ID</h2>
-    <form on:submit|preventDefault={handleSubmit} class="grid gap-4">
+  <section class="py-6">
+    <h2 class="px-6 text-violet-200 font-medium mb-2">Generate ID</h2>
+    <form on:submit|preventDefault={handleSubmit} class="grid gap-5">
       <div
-        class="flex gap-2 w-full rounded-lg border border-zinc-600 bg-zinc-700 outline-none shadow-md"
+        class="flex gap-2 w-full rounded-3xl bg-zinc-800 outline-none shadow-md"
       >
         <!-- "tabular-nums" forces numbers to have the same width -->
         <!-- when generating numbers, it won't shift around -->
         <input
-          class="tabular-nums text-sm text-violet-200 p-2.5 w-full bg-transparent outline-none border-0 focus:ring-0"
+          class="tabular-nums text-xl text-center p-3 w-full bg-transparent outline-none border-0 focus:ring-0"
           name="generatedThaiId"
           type="text"
           value={generatedId}
@@ -32,7 +32,7 @@
       </div>
       <button
         type="submit"
-        class="text-sm font-medium text-white rounded-lg border border-violet-600 bg-violet-500 p-2.5 w-full shadow-md hover:bg-violet-400 hover:border-violet-500 hover:text-white"
+        class="font-medium text-white rounded-full border border-violet-600 bg-violet-500 p-3 w-full shadow-md hover:bg-violet-400 hover:border-violet-500 hover:text-white"
         >Generate</button
       >
     </form>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Thai ID gen',
+        short_name: 'TH ID gen',
+        description: 'Generate Thai ID numbers',
+        // Must match "theme-color" in index.html
+        theme_color: '#8063F5',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
